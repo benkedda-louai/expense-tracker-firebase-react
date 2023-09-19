e9dc807d79114c6a8e29acbc79732aaf0ceecaf2
benkedda-louai <130980852+benkedda-louai@users.noreply.github.com>
2023-09-19 10:14:56 +0100

    m

--- src/hooks/useGetUserInfo.js
+++ src/hooks/useGetUserInfo.js
@@ -1,4 +1,6 @@
 export const useGetUserInfo = () => {
     const { name, profilePhoto, userId, isAuth } = JSON.parse(localStorage.getItem("auth"))||{};
-    return { name, profilePhoto, userId, isAuth };
+    return { name, profilePhoto, userId, isAuth }; 
+
+    
 }
